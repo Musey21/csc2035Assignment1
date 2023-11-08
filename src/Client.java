c27e7c34afe019aeff543759b0d5987a25dde44b
jonny <jonnymuse@hotmail.co.uk>
2023-11-08 23:45:48 +0000

    working sendFileNormal and sendFileWithTimeOut

--- src/Client.java
+++ src/Client.java
@@ -1,9 +1,8 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
+import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.Scanner;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectOutputStream;
 
 
 public class Client {
@@ -30,7 +29,7 @@ public class Client {
 	}	
 
 	/* calculate the segment checksum by adding the payload */
-	public int checksum(String content, Boolean corrupted)
+	public int checksum(String content, boolean corrupted)
 	{
 		if (!corrupted)  
 		{
@@ -143,19 +142,27 @@ public class Client {
 	/* TODO: send metadata (file size and file name to create) to the server 
 	 * outputFile: is the name of the file that the server will create
 	*/
+	// Sends meta data information over the network using UPD protocol
 	public void sendMetaData(int portNumber, InetAddress IPAddress, File file, String outputFile) throws IOException {
 		MetaData metaData = new MetaData();
-		metaData.setName(file.getName()); // set the name to the name of the file
+		metaData.setName(outputFile); // set the name to the name of the file
 		metaData.setSize((int) file.length()); // set the size to the length of the file
 
+		// Create a ByteArrayOutputStream to hold the serialized object
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		// Create an ObjectOutputStream to write objects to the ByteArrayOutputStream
 		ObjectOutputStream objectStream = new ObjectOutputStream(outputStream);
+		// Write the metadata object to the ObjectOutputStream
 		objectStream.writeObject(metaData);
 
+		// Convert the data in the ByteArrayOutputStream to a byte array
 		byte[] data = outputStream.toByteArray();
+		// Create a DatagramPacket to be sent over the network
 		DatagramPacket sentPacket = new DatagramPacket(data, data.length, IPAddress, portNumber);
+		// Send the packet through the socket
 		socket.send(sentPacket);
 
+		// Print the metadata information that has been sent
 		System.out.println("SENDER: Meta data is sent: (file name, size): ( "+ metaData.getName()+", " + metaData.getSize()+")");
 		System.out.println("------------------------------------------------------------------");
 		System.out.println("------------------------------------------------------------------");
@@ -165,16 +172,183 @@ public class Client {
 
 
 	/* TODO: Send the file to the server without corruption*/
-	public void sendFileNormal(int portNumber, InetAddress IPAddress, File file) {
-		exitErr("sendFileNormal is not implemented");
-	} 
+	public void sendFileNormal(int portNumber, InetAddress IPAddress, File file) throws IOException {
+		DatagramSocket socket = new DatagramSocket();
+
+		// Reading the file content as byte array
+		byte[] fileContent = Files.readAllBytes(file.toPath());
+
+		// Create segments and send them
+		int segmentSize = 4;
+		for (int i = 0; i < fileContent.length; i += segmentSize) {
+			byte[] segmentData = Arrays.copyOfRange(fileContent, i, Math.min(i + segmentSize, fileContent.length));
+
+			// Create a segment object
+			Segment segment = new Segment();
+			segment.setSize(segmentData.length);
+			segment.setSq(i / segmentSize);
+			segment.setType(SegmentType.Data);
+			segment.setPayLoad(new String(segmentData));
+
+			//Calculate checksum
+			int calculatedChecksum = checksum(new String(segmentData), true);
+			segment.setChecksum(calculatedChecksum);
+
+			// Create a ByteArrayOutputStream to hold the serialized object
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			// Create an ObjectOutputStream to write objects to the ByteArrayOutputStream
+			ObjectOutputStream objectStream = new ObjectOutputStream(outputStream);
+			// Write the segment object to the ObjectOutputStream
+			objectStream.writeObject(segment);
+
+			// Convert the data in the ByteArrayOutputStream to a byte array
+			byte[] data = outputStream.toByteArray();
+			// Create a DatagramPacket to be sent over the network
+			DatagramPacket sentPacket = new DatagramPacket(data, data.length, IPAddress, portNumber);
+			// Send the packet through the socket
+			socket.send(sentPacket);
+
+			// Print the segment information that has been sent
+			//System.out.println("SENDER: Segment is sent: (segment number, size): (" + segment.getSq() + ", " + segment.getSize() + ")");
+			System.out.println("SENDER: Segment sent:(" + segment.getPayLoad() + "), segment number:"+segment.getSq()+", Checksum:"+ segment.getChecksum());
+			System.out.println("SENDER: Waiting for ack");
+
+			byte[] receiveData = new byte[1024];
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+			socket.setSoTimeout(1000);
+
+			try{
+				socket.receive(receivePacket);
+				// Deserialize the received acknowledgment message
+				ObjectInputStream objectInputStream = new ObjectInputStream(new ByteArrayInputStream(receivePacket.getData()));
+				Segment receivedSegment = (Segment) objectInputStream.readObject();
+
+				// Extract the acknowledgment type and sequence number from the received segment
+				SegmentType ackType = receivedSegment.getType();
+				int ackSq = receivedSegment.getSq();
+
+				System.out.println(ackType + " " + ackSq);
+				// Compare the acknowledgment type and sequence number with the expected values
+				if (ackType == SegmentType.Ack && ackSq == segment.getSq()) {
+					System.out.println("SENDER: ACK for segment " + segment.getSq() + " received.");
+				} else {
+					System.out.println("SENDER: Unexpected ACK received.");
+				}
+
+			}catch (SocketTimeoutException e){
+				System.out.println("SENDER: Timeout occurred. Resending segment " + segment.getSq());
+				// Resends segment
+				i -= segmentSize;
+
+			} catch (ClassNotFoundException e) {
+				throw new RuntimeException(e);
+			}
+
+			//System.out.println("SENDER: ACK sq = " + segment.getSq() + " RECIEVED");
+			System.out.println("------------------------------------------------------------------");
+		}
+	}
+
+
+
 
 	/* TODO: This function is essentially the same as the sendFileNormal function
-	 *      except that it resends data segments if no ACK for a segment is 
+	 *      except that it resends data segments if no ACK for a segment is
 	 *      received from the server.*/
-	public void sendFileWithTimeOut(int portNumber, InetAddress IPAddress, File file, float loss) {
-		exitErr("sendFileWithTimeOut is not implemented");
-	} 
+	public void sendFileWithTimeOut(int portNumber, InetAddress IPAddress, File file, float loss) throws IOException {
+		DatagramSocket socket = new DatagramSocket();
+
+		// Calls isCorrupted
+		Client client = new Client();
+
+
+
+		// Reading the file content as byte array
+		byte[] fileContent = Files.readAllBytes(file.toPath());
+
+		// Create segments and send them
+		int segmentSize = 4;
+		for (int i = 0; i < fileContent.length; i += segmentSize) {
+			byte[] segmentData = Arrays.copyOfRange(fileContent, i, Math.min(i + segmentSize, fileContent.length));
+
+			// Create a segment object
+			Segment segment = new Segment();
+			segment.setSize(segmentData.length);
+			segment.setSq(i / segmentSize);
+			segment.setType(SegmentType.Data);
+			segment.setPayLoad(new String(segmentData));
+
+			boolean resultIsCorrupted = client.isCorrupted(loss);
+			//Calculate checksum
+			int calculatedChecksum = checksum(new String(segmentData), resultIsCorrupted);
+			segment.setChecksum(calculatedChecksum);
+
+
+			// Create a ByteArrayOutputStream to hold the serialized object
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			// Create an ObjectOutputStream to write objects to the ByteArrayOutputStream
+			ObjectOutputStream objectStream = new ObjectOutputStream(outputStream);
+			// Write the segment object to the ObjectOutputStream
+			objectStream.writeObject(segment);
+
+			// Convert the data in the ByteArrayOutputStream to a byte array
+			byte[] data = outputStream.toByteArray();
+			// Create a DatagramPacket to be sent over the network
+			DatagramPacket sentPacket = new DatagramPacket(data, data.length, IPAddress, portNumber);
+			// Send the packet through the socket
+			socket.send(sentPacket);
+
+			// Print the segment information that has been sent
+			//System.out.println("SENDER: Segment is sent: (segment number, size): (" + segment.getSq() + ", " + segment.getSize() + ")");
+			System.out.println("SENDER: Segment sent:(" + segment.getPayLoad() + "), segment number:"+segment.getSq()+", Checksum:"+ segment.getChecksum());
+
+			if (resultIsCorrupted){
+				String ANSI_RED = "\u001B[31m";
+				String ANSI_RESET = "\u001B[0m";
+				System.out.println(ANSI_RED + "\t\t>>>>>>>Network ERROR: segment checksum is corrupted!<<<<<<<<<");
+				System.out.println("SENDER: Sending segment: sq:" + segment.getSq() + " size:" + segment.getSize() + " checksum:" + segment.getChecksum() + "content(" + segment.getPayLoad() + ")" + ANSI_RESET);
+			}
+			System.out.println("SENDER: Waiting for ack");
+
+			byte[] receiveData = new byte[1024];
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+			socket.setSoTimeout(1000);
+
+			try{
+				socket.receive(receivePacket);
+				// Deserialize the received acknowledgment message
+				ObjectInputStream objectInputStream = new ObjectInputStream(new ByteArrayInputStream(receivePacket.getData()));
+				Segment receivedSegment = (Segment) objectInputStream.readObject();
+
+				// Extract the acknowledgment type and sequence number from the received segment
+				SegmentType ackType = receivedSegment.getType();
+				int ackSq = receivedSegment.getSq();
+
+				System.out.println(ackType + " " + ackSq);
+				// Compare the acknowledgment type and sequence number with the expected values
+				if (ackType == SegmentType.Ack && ackSq == segment.getSq()) {
+					System.out.println("SENDER: ACK for segment " + segment.getSq() + " received.");
+				} else {
+					System.out.println("SENDER: Unexpected ACK received.");
+				}
+
+			}catch (SocketTimeoutException e){
+				System.out.println("SENDER: Timeout occurred. Resending segment " + segment.getSq());
+				// Resends segment
+				i -= segmentSize;
+
+			} catch (ClassNotFoundException e) {
+				throw new RuntimeException(e);
+			}
+
+			//System.out.println("SENDER: ACK sq = " + segment.getSq() + " RECIEVED");
+			System.out.println("------------------------------------------------------------------");
+		}
+	}
 
 
 }
+
+
